30df2d92a1ba5e3f72af1678cce7a0dc0e7e02af
Ніколенко Віталій <vitalii.nikolenko@nure.ua>
2021-10-10 17:56:31 +0300

    Add Movie and WatchHistory model

---
+++ MovieKnight.Web/MovieKnight.DataLayer/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieKnight.DataLayer.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string IMDbId { get; set; }
+    }
+}
---
+++ MovieKnight.Web/MovieKnight.DataLayer/Models/WatchHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieKnight.DataLayer.Models
+{
+    class WatchHistory
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+    }
+}
