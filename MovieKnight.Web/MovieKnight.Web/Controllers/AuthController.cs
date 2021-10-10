c228bead705c4ea371d55320b49051966cb7b6c3
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-10-10 18:37:30 +0300

    Add auth and user controllers (without configured mapper profile)

---
+++ MovieKnight.Web/MovieKnight.Web/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieKnight.BusinessLayer.Services.AuthorizationService;
+using MovieKnight.DataLayer.Models.Auth;
+using System.Threading.Tasks;
+
+namespace MovieKnight.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly BaseAuthorizationService _authorizationService;
+        public AuthController(BaseAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("token")]
+        public async Task<IActionResult> Login([FromBody] AuthSignInModel model)
+        {
+            JWTTokenStatusResult result = await _authorizationService.GenerateTokenAsync(model);
+            if (!result.IsAuthorized) { return NotFound(); }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("user-info")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            string currentUserName = User.Identity.Name;
+            UserAuthInfo userInfo = await _authorizationService.GetUserInfoAsync(currentUserName);
+
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userInfo);
+        }
+    }
+}
---
+++ MovieKnight.Web/MovieKnight.Web/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using MovieKnight.BusinessLayer.Constants;
+using MovieKnight.BusinessLayer.DTOs;
+using MovieKnight.BusinessLayer.Exceptions;
+using MovieKnight.BusinessLayer.Services.User;
+using MovieKnight.DataLayer.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace MovieKnight.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> Get(string username)
+        {
+            return Ok(await _service.GetUserByUsername(username));
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post([FromBody] CreateUserDto model)
+        {
+            model.Role = "Client";
+            try
+            {
+                return Ok(await _service.CreateUserAsync(model));
+            }
+            catch (UsernameAlreadyTakenException)
+            {
+                return BadRequest(AddModelStateError("username", ErrorMessagesConstants.USERNAME_ALREADY_TAKEN));
+            }
+        }
+
+        //[HttpPut]
+        //public async Task<IActionResult> Put([FromBody] UpdateUserModel model)
+        //{
+        //    try
+        //    {
+        //        return Ok(await _service.UpdateUserAsync(model));
+        //    }
+        //    catch (InvalidUserPasswordException)
+        //    {
+        //        return BadRequest(AddModelStateError("password", ErrorMessagesConstants.INVALID_PASSWORD));
+        //    }
+        //    catch (UsernameAlreadyTakenException)
+        //    {
+        //        return BadRequest(AddModelStateError("username", ErrorMessagesConstants.USERNAME_ALREADY_TAKEN));
+        //    }
+        //}
+
+        //[HttpPut("{id:guid}")]
+        //public async Task<IActionResult> DeactivateUser(Guid id)
+        //{
+        //    await _service.DeactivateUser(id);
+        //    return Ok();
+        //}
+
+        [HttpDelete("{id:guid}")]
+        [Authorize(Roles = Role.Admin)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _service.DeleteUser(id);
+            return Ok();
+        }
+
+        private ModelStateDictionary AddModelStateError(String field, String error)
+        {
+            ModelStateDictionary modelState = new ModelStateDictionary();
+            modelState.TryAddModelError(field, error);
+            return modelState;
+        }
+    }
+}
