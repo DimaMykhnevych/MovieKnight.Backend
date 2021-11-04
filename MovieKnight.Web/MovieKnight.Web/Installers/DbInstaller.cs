42a8199bc771c97ad1f8a7347f051b69c8b114e0
Ніколенко Віталій <vitalii.nikolenko@nure.ua>
2021-11-04 18:44:26 +0200

    Added Comments; added date to WatchHistory

--- MovieKnight.Web/MovieKnight.Web/Installers/DbInstaller.cs
+++ MovieKnight.Web/MovieKnight.Web/Installers/DbInstaller.cs
@@ -12,8 +12,10 @@ namespace MovieKnight.Web.Installers
         {
             services.Configure<RemoteMySqlOptions>(configuration.GetSection("ConnectionStrings:Default"));
             string connectionString = configuration["ConnectionStrings:Default"];
+            /*services.AddDbContext<MovieKnightDbContext>(opt =>
+                    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));*/
             services.AddDbContext<MovieKnightDbContext>(opt =>
-                    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+                    opt.UseSqlServer(connectionString));
         }
     }
 }
